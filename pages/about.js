a869f510338e8f4fd8b604c8eb0c8058477fcc6c
Vova Borisenko <vovaborisenko@live.com>
2019-09-20 12:18:04 +0300

    create dynamic pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import MyLayout from "../components/MyLayout"
+
+export default function About() {
+  return (
+    <MyLayout>
+      <p>This is the about page</p>
+    </MyLayout>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 )
 
-export default Index
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  )
+}
